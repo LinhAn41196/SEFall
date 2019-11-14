a472eafbc92cd6234d8148a3e5b7da1502081408
LinhAn41196 <anho@sheridancollege.ca>
2019-11-13 23:09:42 -0500

    Add project files.

---
+++ WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            using(OurContext db = new OurContext())
+            {
+                return View(db.users.ToList());
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OurContext db = new OurContext())
+                {
+                    db.users.Add(user);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = user.Email + " successfully registered.";
+            }
+            else
+            {
+                ModelState.AddModelError("", "Some error occurred");
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+        {
+            using(OurContext db = new OurContext())
+            {
+                var usr = db.users.Where(u => u.Email == user.Email && u.Password == user.Password).FirstOrDefault();
+                if(usr != null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["Email"] = usr.Email.ToString();
+                    return RedirectToAction("LoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Some error occurred");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult LoggedIn()
+        {
+            if(Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ WebApplication2/Models/UserAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Organization name required")]
+        public string OrganizationName { get; set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Email required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Password required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Password does not match")]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+} 
